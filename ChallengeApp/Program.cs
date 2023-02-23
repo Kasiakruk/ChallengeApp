8c009bbcf5357ec69826681680332a56a020f93b
kasiakruk <katmar3713@wp.pl>
2023-02-23 20:14:44 +0100

    DZIEŃ 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// dzień 4
+
+string name = "Kasia ";
+string sex = "jest kobietą ";
+string age = "i ma 30 lat";
+string result = name + sex + age;
+Console.WriteLine(result);
+
+var number1 = 15;
+var number2 = 20;
+if (number1 < number2) 
+{
+Console.WriteLine("Ewa, lat 33");
+}
+else
+{
+    Console.WriteLine('0');
+}
+
+bool isActiv = true;
+isActiv=false;
+var isvalid = 4 == 6;
+Console.WriteLine(isvalid);
