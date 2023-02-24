e7ac127b24283bd41fedd7109e0aeeab3575fe67
kasiakruk <katmar3713@wp.pl>
2023-02-24 22:48:52 +0100

    Dzień 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,23 +1,74 @@
-﻿// dzień 4
+﻿// Dzień 5
 
-string name = "Kasia ";
-string sex = "jest kobietą ";
-string age = "i ma 30 lat";
-string result = name + sex + age;
-Console.WriteLine(result);
+int number = 4566;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-var number1 = 15;
-var number2 = 20;
-if (number1 < number2) 
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char letter in letters)
 {
-Console.WriteLine("Ewa, lat 33");
-}
-else
-{
-    Console.WriteLine('0');
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+
 }
 
-bool isActiv = true;
-isActiv=false;
-var isvalid = 4 == 6;
-Console.WriteLine(isvalid);
+Console.WriteLine("wyniki dla liczby:" + number);
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
